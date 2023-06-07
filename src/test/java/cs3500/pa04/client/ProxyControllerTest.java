03c32fd0b85d4bc809a9bd1dab25e8d576702322
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2023-06-07 12:23:26 -0400

    Mocket Tests Progress

--- src/test/java/cs3500/pa04/client/ProxyControllerTest.java
+++ src/test/java/cs3500/pa04/client/ProxyControllerTest.java
@@ -1,17 +1,124 @@
 package cs3500.pa04.client;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cs3500.pa03.model.player.ArtificialIntelligence;
+import cs3500.pa04.json.JsonUtils;
+import cs3500.pa04.json.MessageJson;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * Tests the proxy controller
+ * Test correct responses for different requests from the socket using a Mock Socket (mocket)
  */
-class ProxyControllerTest {
+public class ProxyControllerTest {
+
+  private ByteArrayOutputStream testLog;
+  private ProxyController controller;
+
 
   /**
-   * Tests the run function in ProxyController
+   * Reset the test log before each test is run.
+   */
+  @BeforeEach
+  public void setup() {
+    this.testLog = new ByteArrayOutputStream(2048);
+    assertEquals("", logToString());
+  }
+
+  /**
+   * Check that the server returns a guess when given a hint.
    */
   @Test
-  void runTest() {
+  public void testVoidForWin() {
+    // Prepare sample message
+    WinJson winJson = new WinJson(true);
+    JsonNode sampleMessage = createSampleMessage("win", winJson);
 
+    // Create the client with all necessary messages
+    Mocket socket = new Mocket(this.testLog, List.of(sampleMessage.toString()));
+
+    // Create a Dealer
+    try {
+      this.controller = new ProxyController(socket, new ArtificialIntelligence("test"));
+    } catch (IOException e) {
+      fail(); // fail if the dealer can't be created
+    }
+
+    // run the dealer and verify the response
+    this.controller.run();
+
+    String expected = "\"void\"\n";
+    assertEquals(expected, logToString());
+  }
+
+  /**
+   * Check that the server returns a guess when given a hint.
+   */
+  @Test
+  public void testGuessForHint() {
+    // Create sample hint
+    HintJson hintJson = new HintJson(HintJson.HIGHER);
+    JsonNode jsonNode = createSampleMessage("hint", hintJson);
+
+    // Create socket with sample input
+    Mocket socket = new Mocket(this.testLog, List.of(jsonNode.toString()));
+
+    // Create a dealer
+    try {
+      this.controller = new ProxyController(socket, new ArtificialIntelligence("test"));
+    } catch (IOException e) {
+      fail(); // fail if the dealer can't be created
+    }
+
+    // Run dealer and verify response.
+    this.controller.run();
+    responseToClass(GuessJson.class);
+  }
+
+  /**
+   * Converts the ByteArrayOutputStream log to a string in UTF_8 format
+   * @return String representing the current log buffer
+   */
+  private String logToString() {
+    return testLog.toString(StandardCharsets.UTF_8);
+  }
+
+  /**
+   * Try converting the current test log to a string of a certain class.
+   *
+   * @param classRef Type to try converting the current test stream to.
+   * @param <T>      Type to try converting the current test stream to.
+   */
+  private <T> void responseToClass(@SuppressWarnings("SameParameterValue") Class<T> classRef) {
+    try {
+      JsonParser jsonParser = new ObjectMapper().createParser(logToString());
+      jsonParser.readValueAs(classRef);
+      // No error thrown when parsing to a GuessJson, test passes!
+    } catch (IOException e) {
+      // Could not read
+      // -> exception thrown
+      // -> test fails since it must have been the wrong type of response.
+      fail();
+    }
+  }
+
+  /**
+   * Create a MessageJson for some name and arguments.
+   * @param messageName name of the type of message; "hint" or "win"
+   * @param messageObject object to embed in a message json
+   * @return a MessageJson for the object
+   */
+  private JsonNode createSampleMessage(String messageName, Record messageObject) {
+    MessageJson messageJson = new MessageJson(messageName, JsonUtils.serializeRecord(messageObject));
+    return JsonUtils.serializeRecord(messageJson);
   }
 }
